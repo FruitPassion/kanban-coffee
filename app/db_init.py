3ca13b9c307d5eb494febe1d87f6e706b2fbdda8
FruitPassion <gamehotdl@gmail.com>
2024-04-28 14:49:10 +0200

    Feat: include metadata table creation

--- app/db_init.py
+++ app/db_init.py
@@ -4,6 +4,7 @@ from app import create_app
 
 app = create_app("dev")
 with app.app_context():
-    from model.shared_model import db
+    from model.shared_model import Base, db
 
     db.create_all()
+    Base.metadata.create_all(bind=db.engine)
